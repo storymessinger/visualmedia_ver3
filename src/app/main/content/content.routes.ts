799d14036f7f9346e88306a940660c967c54b014
Jun <Jun@Junui-MacBook-Pro.local>
2017-03-25 11:27:26 +0900

    remove content.component

--- src/app/main/content/content.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from "@angular/router";
-
-export const CONTENT_ROUTES: Routes = [
-    // { path: 'detail', component: UserDetailComponent, canActivate: [UserDetailGuard] },
-    // { path: 'edit', component: UserEditComponent, canDeactivate: [UserEditGuard] }
-];
